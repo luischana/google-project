6c55b209c5dad3ecf058ec36a0f9efba34a0487c
luischana <lc@sem-wolf.org>
2020-07-23 16:03:59 +0300

    Supports the return of five objects by the five_sentence function

--- online.py
+++ online.py
@@ -1,4 +1,5 @@
 from offline import dict,listAllSentences
+from auto_complete_data import AutocompleteData
 import string
 
 
@@ -14,10 +15,10 @@ def check_list(listSentences, sentence, score):
 def replace_str(str, listSentences):
     for char in string.ascii_letters:
         for j in range(len(str)):
-            if dict.get(str[:j] + char + str[j + 1:]):
-                for k in dict[str[:j] + char + str[j + 1:]]:
+            if dict.get(str[:j]+char+str[j+1:]):
+                for k in dict[str[:j]+char+str[j+1:]]:
                     if char not in str[j]:
-                        score = len(str) * 2 - 5 + j if j < 5 else len(str) - 1
+                        score = len(str)*2-5+j if j < 5 else len(str)-1
                         if check_list(listSentences, k, score):
                             listSentences.append({"sentence": k, "score": score})
     return listSentences
@@ -25,10 +26,10 @@ def replace_str(str, listSentences):
 
 def add_str(str, listSentences):
     for char in string.printable[10:65]:
-        for j in range(len(str) + 1):
-            if dict.get(str[:j] + char + str[j:]):
-                for k in dict[str[:j] + char + str[j:]]:
-                    score = len(str) * 2 - 10 + 2 * j if j < 4 else len(str) * 2 - 2
+        for j in range(len(str)+1):
+            if dict.get(str[:j]+char+str[j:]):
+                for k in dict[str[:j]+char+str[j:]]:
+                    score = len(str)*2-10+2*j if j < 4 else len(str)*2-2
                     if check_list(listSentences, k, score):
                         listSentences.append({"sentence": k, "score": score})
     return listSentences
@@ -36,13 +37,20 @@ def add_str(str, listSentences):
 
 def remove_str(str, listSentences):
     for j in range(len(str)):
-        if dict.get(str[:j] + str[j + 1:]):
-            if str[:j] + str[j + 1:] != str:
-                for k in dict[str[:j] + str[j + 1:]]:
-                    score = len(str) * 2 - 10 + 2 * j if j < 4 else len(str) * 2 - 2
+        if dict.get(str[:j]+str[j+1:]):
+            if str[:j]+str[j+1:] != str:
+                for k in dict[str[:j]+str[j+1:]]:
+                    score = len(str)*2-10+2 *j if j < 4 else len(str)*2-2
                     if check_list(listSentences, k, score):
                         listSentences.append({"sentence": k, "score": score})
     return listSentences
+
+
+def five_sentences(listSentences):
+    return [AutocompleteData(listAllSentences[listSentences[x]["sentence"]]["completed_sentence"],
+                             listAllSentences[listSentences[x]["sentence"]]["source_text"],
+                             listAllSentences[listSentences[x]["sentence"]]["offset"],
+                             listSentences[x]["score"]) for x in range(len(listSentences)) if x < 5]
 
 
 def get_best_k_completions(str):
@@ -53,19 +61,15 @@ def get_best_k_completions(str):
             listSentences.append({"sentence": i, "score": len(str)*2})
 
         if len(listSentences) >= 5:
-            listSentences = sorted(listSentences, key=lambda a: listAllSentences[a["sentence"]])
-
-            return listSentences[:5]
+            listSentences = sorted(listSentences, key=lambda a: listAllSentences[a["sentence"]]["completed_sentence"])
+            return five_sentences(listSentences)
 
     listSentences = replace_str(str, listSentences)
     listSentences = add_str(str, listSentences)
     listSentences = remove_str(str, listSentences)
 
     listSentences = sorted(listSentences, key=lambda a: a["score"], reverse=True)
-
-    if len(listSentences) <= 5:
-        return listSentences
+    return five_sentences(listSentences)
 
     #listSentences = sorted(listSentences, key=lambda a: listAllSentences[a["sentence"]])
     #listSentences = listSentences[:5]
-    return listSentences[:5]
