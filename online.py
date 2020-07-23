b5fd18a4a1b94a5a869a518f34c5b8413b3425c6
luischana <lc@sem-wolf.org>
2020-07-23 05:10:09 +0300

    function replace_str

--- online.py
+++ online.py
@@ -1,8 +1,32 @@
+from offline import dict,listAllSentences
+import string
+
+
+def check_list(listSentences, sentence, score):
+    for i in range(len(listSentences)):
+        if sentence == listSentences[i]["sentence"]:
+            if score > listSentences[i]["score"]:
+                listSentences[i]["score"] = score
+            return 0
+    return 1
+
+
 def replace_str(str, listSentences):
+    for char in string.ascii_letters:
+        for j in range(len(str)):
+            if dict.get(str[:j] + char + str[j + 1:]):
+                for k in dict[str[:j] + char + str[j + 1:]]:
+                    if char not in str[j]:
+                        score = len(str) * 2 - 5 + j if j < 5 else len(str) - 1
+                        if check_list(listSentences, k, score):
+                            listSentences.append({"sentence": k, "score": score})
+    return listSentences
 
 def add_str(str, listSentences):
+    return
 
 def remove_str(str, listSentences):
+    return
 
 def get_best_k_completions(str):
     listSentences = []
