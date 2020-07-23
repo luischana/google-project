c74a3bb964d371742f983e6723c2d9ab459a1260
luischana <lc@sem-wolf.org>
2020-07-23 15:57:12 +0300

    Change listAllSentences to a list that holds AutocompleteData objects

--- offline.py
+++ offline.py
@@ -1,24 +1,18 @@
 dict = dict()
 listAllSentences = []
 
+
 with open("about.txt") as the_file:
-    listAllSentences = the_file.read().split("\n")
-
-
-# with open("about.txt") as the_file:
-#     allSentences = the_file.read().split("\n")
-#     for i in range(allSentences):
-#         listAllSentences.append({"completed_sentence": allSentences[i], "source_text": "about.txt", "offset": i})
+     allSentences = the_file.read().split("\n")
+     for i in range(len(allSentences)):
+         listAllSentences.append({"completed_sentence": allSentences[i], "source_text": "about.txt", "offset": i})
 
 
 def init_data():
-    for str in range(len(listAllSentences)):
-         for j in range(len(listAllSentences[str])):
-             dict.setdefault((listAllSentences[str])[:j+1], set()).add(str)
+     for sentence in range(len(listAllSentences)):
+        res = [listAllSentences[sentence]["completed_sentence"][i: j]
+               for i in range(len(listAllSentences[sentence]["completed_sentence"]))
+               for j in range(i + 1, len(listAllSentences[sentence]["completed_sentence"]) + 1)]
 
-         listWords = listAllSentences[str].split(" ")
-
-         for word in range(len(listWords)):
-             for j in range(len(listWords[word])):
-                 dict.setdefault((listWords[word])[:j+1], set()).add(str)
-
+        for substring in range(len(res)):
+            dict.setdefault(res[substring], set()).add(sentence)
