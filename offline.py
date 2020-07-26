c9c688e740d34846bdd2f8ce60dd651137a82e5b
luischana <lc@sem-wolf.org>
2020-07-26 03:07:22 +0300

    Arranging code

--- offline.py
+++ offline.py
@@ -1,20 +1,20 @@
 import os, re
-dictFile = dict()
-dict = dict()
-listAllSentences = []
+dict_file = dict()
+dict_substring = dict()
+list_all_sentences = []
 
 
 def init_list_dict(path, ind):
     with open(path, encoding='UTF-8') as the_file:
         temp_list_string = the_file.read().split("\n")
     for index_line, line in enumerate(temp_list_string, 1):
-        listAllSentences.append({"completed_sentence": line, "source_text": ind, "offset": index_line})
+        list_all_sentences.append({"completed_sentence": line, "source_text": ind, "offset": index_line})
 
 
 def read_from_file():
     for dirpath, dirnames, files in os.walk('c-api', topdown=True):
         for ind, file_name in enumerate(files, 1):
-            dictFile[ind] = file_name
+            dict_file[ind] = file_name
             init_list_dict(dirpath + "\\" + file_name, ind)
 
 
@@ -23,9 +23,9 @@ def ignore_char(sentence):
 
 
 def init_data():
-    for sentence in range(len(listAllSentences)):
-        sentene_fixed = ignore_char(listAllSentences[sentence]["completed_sentence"])
-        res = [sentene_fixed[i: j] for i in range(len(sentene_fixed)) for j in range(i + 1, len(sentene_fixed) + 1)]
+    for sentence in range(len(list_all_sentences)):
+        sentence_fixed = ignore_char(list_all_sentences[sentence]["completed_sentence"])
+        res = [sentence_fixed[i: j] for i in range(len(sentence_fixed)) for j in range(i + 1, len(sentence_fixed) + 1)]
 
         for substring in range(len(res)):
-            dict.setdefault(res[substring], set()).add(sentence)
+            dict_substring.setdefault(res[substring], set()).add(sentence)
--- online.py
+++ online.py
@@ -1,75 +1,76 @@
-from offline import dict, dictFile, listAllSentences
+from offline import dict_substring, dict_file, list_all_sentences
 from auto_complete_data import AutocompleteData
 import string
 
 
-def check_list(listSentences, sentence, score):
-    for i in range(len(listSentences)):
-        if sentence == listSentences[i]["sentence"]:
-            if score > listSentences[i]["score"]:
-                listSentences[i]["score"] = score
-            return 0
-    return 1
+def check_sentence(list_sentences, sentence, score):
+    for i in range(len(list_sentences)):
+        if sentence == list_sentences[i]["sentence"]:
+            if score > list_sentences[i]["score"]:
+                list_sentences[i]["score"] = score
+            return list_sentences
+
+    list_sentences.append({"sentence": sentence, "score": score})
+    return list_sentences
 
 
-def replace_str(str, listSentences):
-    for char in string.ascii_letters:
-        for j in range(len(str)):
-            if dict.get(str[:j]+char+str[j+1:]):
-                for k in dict[str[:j]+char+str[j+1:]]:
-                    if char not in str[j]:
-                        score = len(str)*2-5+j if j < 5 else len(str)-1
-                        if check_list(listSentences, k, score):
-                            listSentences.append({"sentence": k, "score": score})
-    return listSentences
+def replace_letter(str, list_sentences, score):
+    for i in range(len(str)):
+        for letter in string.ascii_letters:
+            if dict_substring.get(str[:i]+letter+str[i+1:]):
+                for sentence in dict_substring[str[:i]+letter+str[i+1:]]:
+                    if letter not in str[i]:
+                        score = score-5+i if i < 5 else score-1
+                        check_sentence(list_sentences, sentence, score)
+
+    return list_sentences
 
 
-def add_str(str, listSentences):
-    for char in string.printable[10:65]:
-        for j in range(len(str)+1):
-            if dict.get(str[:j]+char+str[j:]):
-                for k in dict[str[:j]+char+str[j:]]:
-                    score = len(str)*2-10+2*j if j < 4 else len(str)*2-2
-                    if check_list(listSentences, k, score):
-                        listSentences.append({"sentence": k, "score": score})
-    return listSentences
+def add_letter(str, list_sentences, score):
+    for i in range(len(str)+1):
+        for letter in string.ascii_letters:
+            if dict_substring.get(str[:i]+letter+str[i:]):
+                for sentence in dict_substring[str[:i]+letter+str[i:]]:
+                    score = score-10+2*i if i < 4 else score-2
+                    check_sentence(list_sentences, sentence, score)
+
+    return list_sentences
 
 
-def remove_str(str, listSentences):
-    for j in range(len(str)):
-        if dict.get(str[:j]+str[j+1:]):
-            if str[:j]+str[j+1:] != str:
-                for k in dict[str[:j]+str[j+1:]]:
-                    score = len(str)*2-10+2 *j if j < 4 else len(str)*2-2
-                    if check_list(listSentences, k, score):
-                        listSentences.append({"sentence": k, "score": score})
-    return listSentences
+def remove_letter(str, list_sentences, score):
+    for i in range(len(str)):
+        if dict_substring.get(str[:i]+str[i+1:]):
+            if str[:i]+str[i+1:] != str:
+                for sentence in dict_substring[str[:i]+str[i+1:]]:
+                    score = score-10+2*i if i < 4 else score-2
+                    check_sentence(list_sentences, sentence, score)
+
+    return list_sentences
 
 
-def five_sentences(listSentences):
-    return [AutocompleteData(listAllSentences[listSentences[x]["sentence"]]["completed_sentence"],
-                             dictFile[listAllSentences[listSentences[x]["sentence"]]["source_text"]],
-                             listAllSentences[listSentences[x]["sentence"]]["offset"],
-                             listSentences[x]["score"]) for x in range(len(listSentences)) if x < 5]
+def five_sentences(list_sentences):
+    return [AutocompleteData(list_all_sentences[list_sentences[i]["sentence"]]["completed_sentence"],
+                             dict_file[list_all_sentences[list_sentences[i]["sentence"]]["source_text"]],
+                             list_all_sentences[list_sentences[i]["sentence"]]["offset"],
+                             list_sentences[i]["score"]) for i in range(len(list_sentences)) if i < 5]
 
 
 def get_best_k_completions(str):
-    listSentences = []
+    list_sentences = []
+    score = len(str)*2
 
-    if dict.get(str):
-        for i in list(dict[str]):
-            listSentences.append({"sentence": i, "score": len(str)*2})
+    if dict_substring.get(str):
+        for sentence in list(dict_substring[str]):
+            list_sentences.append({"sentence": sentence, "score": score})
 
-        if len(listSentences) >= 5:
-            listSentences = sorted(listSentences, key=lambda a: listAllSentences[a["sentence"]]["completed_sentence"])
-            return five_sentences(listSentences)
+    if len(list_sentences) >= 5:
+        list_sentences = sorted(list_sentences, key=lambda a: list_all_sentences[a["sentence"]]["completed_sentence"])
 
-    listSentences = replace_str(str, listSentences)
-    listSentences = add_str(str, listSentences)
-    listSentences = remove_str(str, listSentences)
+    else:
+        replace_letter(str, list_sentences, score)
+        add_letter(str, list_sentences, score)
+        remove_letter(str, list_sentences, score)
 
-    listSentences = sorted(listSentences, key=lambda a: a["score"], reverse=True)
-    return five_sentences(listSentences)
+        list_sentences = sorted(list_sentences, key=lambda a: a["score"], reverse=True)
 
-    #listSentences = sorted(listSentences, key=lambda a: listAllSentences[a["sentence"]])
-    #listSentences = listSentences[:5]
+    return five_sentences(list_sentences)
